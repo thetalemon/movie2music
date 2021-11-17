4bd3b08771f4c4854c4f9c332137227e374b08bb
thetalemon <sasami0314@gmail.com>
2021-11-17 22:28:16 +0900

    feat: kick make music in api

--- backend/index.py
+++ backend/index.py
@@ -4,19 +4,21 @@
 from __future__ import print_function
 from flask import Flask, send_file
 from flask_cors import cross_origin
+from makeMusic import create_music
 
 app = Flask(__name__)
 XLSX_MIMETYPE = 'audio/midi'
 
-
 @app.route("/")
 @cross_origin(supports_credentials=True) 
 def index():
+  create_music()
+
   downloadFileName = 'sample.mid'
   downloadFile = './sample.mid'
 
   return send_file(downloadFile, as_attachment = True, \
-      attachment_filename = downloadFileName, \
+      download_name = downloadFileName, \
       mimetype = XLSX_MIMETYPE)
 
 if __name__ == "__main__":
---
+++ backend/makeMusic.py
@@ -0,0 +1,18 @@
+import pretty_midi
+import os
+
+def create_music():
+  cello_c_chord = pretty_midi.PrettyMIDI()
+  cello_program = pretty_midi.instrument_name_to_program('Cello')
+  cello = pretty_midi.Instrument(program=cello_program)
+
+  time = 0.0
+
+  for note_name in ['C5', 'D5', 'E5', 'F5', 'G5', 'A5', 'B5', 'C6']:
+    note_number = pretty_midi.note_name_to_number(note_name)
+    note = pretty_midi.Note(
+        velocity=100, pitch=note_number, start=time, end=(time + .5))
+    cello.notes.append(note)
+    time = time + .5
+  cello_c_chord.instruments.append(cello)
+  cello_c_chord.write(os.path.dirname(__file__) + '/sample.mid')
