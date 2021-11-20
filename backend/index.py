2db3b2be75e05718fdf946acc0bf2976137b2f16
thetalemon <sasami0314@gmail.com>
2021-11-20 21:59:16 +0900

    feat: send path to backend

--- backend/index.py
+++ backend/index.py
@@ -2,14 +2,15 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
-from flask import Flask, send_file
-from flask_cors import cross_origin
+from flask import Flask, send_file, request
+from flask_cors import CORS, cross_origin
 from makeMusic import create_music
 
 app = Flask(__name__)
+CORS(app, support_credentials=True, resources={r"/*": {"credentials": True, "origins": "http://127.0.0.1:8000"}})
 XLSX_MIMETYPE = 'audio/midi'
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 @cross_origin(supports_credentials=True) 
 def index():
   create_music()
@@ -20,6 +21,11 @@ def index():
   return send_file(downloadFile, as_attachment = True, \
       download_name = downloadFileName, \
       mimetype = XLSX_MIMETYPE)
+
+@app.route("/uploadFile", methods=["POST"])
+@cross_origin(supports_credentials=True) 
+def uploadFile():  
+  return request.form["file"]
 
 if __name__ == "__main__":
   app.run(host="127.0.0.1", port=5000)
